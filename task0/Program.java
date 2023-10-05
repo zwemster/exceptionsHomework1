0cc04d2eb86321496f059ce83c02667a7e81fefe
zwemster <104361935+zwemster@users.noreply.github.com>
2023-10-05 18:39:38 +0300

    task0 solved

--- task0/Program.java
+++ task0/Program.java
@@ -1,4 +1,24 @@
 package task0;
-public class Program {
 
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        float userInput = floatNumberInput("Input a float number: ");
+        System.out.println("You input: " + userInput);
+    }
+
+    public static float floatNumberInput(String prompt) {
+        float inputNumber = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print(prompt);
+            inputNumber = Float.parseFloat(scanner.nextLine());
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Invalid format.");
+        }
+        return inputNumber;
+    }
 }
