c13841a73fc11c9fbd85019f4681655c2e2454ed
zwemster <104361935+zwemster@users.noreply.github.com>
2023-10-05 19:30:38 +0300

    task1 solved

---
+++ task1/Program.java
@@ -0,0 +1,19 @@
+package task1;
+
+public class Program {
+    /**
+     *
+     * Данная конструкция try-catch не требует никаких изменений.
+     * Деление на 0 происходит внутри блока "try", поэтому исключение будет
+     * поймано.
+
+    try {
+        int d = 0;
+        double catchedRes1 = intArray[8] / d;
+        System.out.println("catchedRes1 = " + catchedRes1);
+    } catch (ArithmeticException e) {
+        System.out.println("Catching exception: " + e);
+    }
+
+    */
+}
